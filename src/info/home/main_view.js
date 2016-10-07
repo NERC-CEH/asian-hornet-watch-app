956590645f1c70b79d45852635cf9c6f5e3e105d
kazlauskis <karolis@kazlauskis.com>
2016-10-07 11:54:18 +0100

    Add app home page logo

--- src/info/home/main_view.js
+++ src/info/home/main_view.js
@@ -5,6 +5,7 @@ import Marionette from 'backbone.marionette';
 import JST from 'JST';
 import './styles.scss';
 import './images/app_logo.png';
+import './images/background.png';
 
 export default Marionette.View.extend({
   id: 'home',
