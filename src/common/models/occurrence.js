aa54853022490ecb19af1047d118d8e16ea3dc3d
kazlauskis <karolis@kazlauskis.com>
2018-10-28 21:01:22 +0000

    Fixed image paths

--- src/common/models/occurrence.js
+++ src/common/models/occurrence.js
@@ -4,7 +4,7 @@ import appModel from 'app_model';
 import ImageModel from './image';
 
 export default Indicia.Occurrence.extend({
-  Image: ImageModel,
+  Media: ImageModel,
 
   keys: CONFIG.indicia.occurrence, // warehouse attribute keys
 
