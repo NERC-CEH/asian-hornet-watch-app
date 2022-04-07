11743cbd740195f240cc2324578e32a5d15dfdcc
Vilius Stankaitis <vilius@stankaitis.com>
2022-04-07 10:37:30 +0300

    Init capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'uk.ac.ceh.hornets',
+  appName: 'Asian Hornet Watch',
+  webDir: 'build',
+  bundledWebRuntime: false,
+};
+
+export default config;
