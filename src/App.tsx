3ce4d72c5e51b62e180c8795cc586413532fee1a
Vilius Stankaitis <vilius@stankaitis.com>
2022-04-08 09:53:28 +0300

    Clean up

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Route, Redirect } from 'react-router-dom';
 import { IonApp, IonRouterOutlet } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import { observer } from 'mobx-react';
-import Menu from 'Components/Menu';
 import Home from './Home';
 import Info from './Info/router';
 
@@ -12,18 +11,15 @@ const HomeRedirect = () => {
 };
 
 const App = () => (
-  <React.StrictMode>
-    <IonApp>
-      <IonReactRouter>
-        <Menu />
-        <IonRouterOutlet id="main">
-          <Route exact path="/" component={HomeRedirect} />
-          <Route path="/home" component={Home} />
-          {Info}
-        </IonRouterOutlet>
-      </IonReactRouter>
-    </IonApp>
-  </React.StrictMode>
+  <IonApp>
+    <IonReactRouter>
+      <IonRouterOutlet id="main">
+        <Route exact path="/" component={HomeRedirect} />
+        <Route path="/home" component={Home} />
+        {Info}
+      </IonRouterOutlet>
+    </IonReactRouter>
+  </IonApp>
 );
 
 export default observer(App);
