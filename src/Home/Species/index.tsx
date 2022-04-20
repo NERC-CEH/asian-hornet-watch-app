fc87df0a86b15c0a90239e8885ba29419a143ee9
Vilius Stankaitis <vilius@stankaitis.com>
2022-04-20 09:38:41 +0300

    Add guide page

---
+++ src/Home/Species/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Page } from '@flumens';
+import SpeciesList from 'common/Components/SpeciesList';
+import './styles.scss';
+
+function Guide() {
+  return (
+    <Page id="guide">
+      <SpeciesList />
+    </Page>
+  );
+}
+
+export default Guide;
